7bc424666820d60bc376b346f1f229fffe800edc
Vanessa Ching <vanessaching@vanessas-macbook-air.local>
2026-01-06 03:21:18 -0500

    Modified default path within vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,17 @@
 import { defineConfig } from 'vite'
 import path from 'path'
-import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: '/personal-website/',
   plugins: [
     react(),
-    tailwindcss(),
+    tailwindcss()
   ],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
+      '@': path.resolve(__dirname, './src')
+    }
+  }
 })
