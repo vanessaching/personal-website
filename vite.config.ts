bca7dece0d67a2d75ee9d14a6632382036047dba
Vanessa Ching <vanessaching@vanessas-macbook-air.local>
2026-01-06 03:37:28 -0500

    Modified index.html and vite.config.ts to properly mount React

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,14 @@
-import { defineConfig } from 'vite'
-import path from 'path'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
+import path from 'path';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
-export default defineConfig({
-  base: '/personal-website/',
-  plugins: [
-    react(),
-    tailwindcss()
-  ],
+export default defineConfig(({ command }) => ({
+  base: command === 'build' ? '/personal-website/' : '/',
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src')
-    }
-  }
-})
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+}));
